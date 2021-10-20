8db957a3cb86f981ce56a2da70e8c9cd5c1543c5
FMSHY <68935791+FMSHY@users.noreply.github.com>
2021-10-20 22:31:42 +0700

    Add files via upload

---
+++ jquery_ariqbimo_5026201071/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#redcolorchange").click(function(){
+      $("#color").css('background-color', 'crimson');
+    });
+
+    $("#greencolorchange").click(function(){
+        $("#color").css('background-color', '#69ea07');
+      });
+  });
